ca0294fac571ddf1ec22071963eb595833ad6323
FarhanaRafi <118438210+FarhanaRafi@users.noreply.github.com>
2022-11-28 14:21:45 +0100

    script in html

---
+++ index.js
@@ -0,0 +1,12 @@
+let name = "Farhana "
+let lastName = "Kodenchery Mohammed Rafi"
+
+let fullName = name +lastName;
+console.log("Hello, my name is " , fullName);
+
+let age = 27;
+let currentYear = 2022;
+let birthYear = currentYear - age;
+
+console.log("My birth year is:" , birthYear)
+
