d8fb840c691322d5a3499473a1eec6aebc0f8ae8
FarhanaRafi <118438210+FarhanaRafi@users.noreply.github.com>
2022-11-28 15:58:51 +0100

    console

--- recipe.js
+++ recipe.js
@@ -3,22 +3,33 @@ let cheese = 50;
 let blackPepper = 4;
 let guanciale = 250;
 let pasta = 400;
-let water = 1500;
+let water = 2500;
 let pastaStarch = 30;
 
+
 let carbonara = eggYolk + cheese;
+console.log("Mixing egg yolks and cheese:");
+console.log(carbonara);
 
 let roastedBlackPepper = blackPepper / 2;
 
 carbonara += roastedBlackPepper;
+console.log("\nAdding small amount of roasted black pepper:");
+console.log(carbonara);
 
 carbonara += guanciale;
+console.log("\nAdding guanciale to the mixture:");
+console.log(carbonara);
 
 carbonara += pasta + water * 0.1;
+console.log("\nAdding cooked pasta after absorption by water:");
+console.log(carbonara);
 
 carbonara += pastaStarch;
+console.log("\nMixing pasta with the prepared sauce:");
+console.log(carbonara);
 
 carbonara += roastedBlackPepper;
 
-console.log("The sum of all the ingredients for carbonara is:")
+console.log("\nThe sum of all the ingredients for carbonara is:")
 console.log(carbonara)
